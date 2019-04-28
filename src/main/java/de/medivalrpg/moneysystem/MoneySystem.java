e46fd28a9231d0f8f6348d67310c7679c53888c9
Daschi <daschi.03@icloud.com>
2019-04-28 17:24:35 +0200

    MoneySystem objective added.

--- src/main/java/de/medivalrpg/moneysystem/MoneySystem.java
+++ src/main/java/de/medivalrpg/moneysystem/MoneySystem.java
@@ -4,15 +4,18 @@ import de.medivalrpg.MedivalRPG;
 
 public class MoneySystem {
 
+    //System objectives
     private MedivalRPG plugin;
     private MoneyUtil moneyUtil;
 
     public MoneySystem(MedivalRPG plugin) {
+        //Initialize system objectives
         this.plugin = plugin;
         this.moneyUtil = new MoneyUtil();
     }
 
     public void initialize() {
+        //Register
         //TODO register
     }
 
--- src/main/java/de/medivalrpg/moneysystem/MoneyUtil.java
+++ src/main/java/de/medivalrpg/moneysystem/MoneyUtil.java
@@ -1,7 +1,34 @@
 package de.medivalrpg.moneysystem;
 
-import de.medivalrpg.MedivalRPG;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
 
 public class MoneyUtil {
+
+    public void getMoneyFromPlayerInventory(Player player) {
+        if (player != null) {
+            Inventory inventory = player.getInventory();
+            ItemStack[] itemStacks = inventory.getContents();
+            int playerMoney = 0;
+            for (ItemStack itemStack : itemStacks) {
+                if (itemStack != null) {
+                    switch (itemStack.getType()) {
+                        case GOLD_NUGGET:
+                            playerMoney++;
+                            break;
+                        case GOLD_INGOT:
+                            playerMoney += 9;
+                            break;
+                        case GOLD_BLOCK:
+                            playerMoney += 81;
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
+        }
+    }
 
 }
