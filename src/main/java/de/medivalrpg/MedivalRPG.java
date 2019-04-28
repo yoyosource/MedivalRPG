f7f5756087d1606195572efbfbbff203299b5d51
jojo <yoyosource@nidido.de>
2019-04-28 16:42:15 +0200

    Drink System added

--- src/main/java/de/medivalrpg/MedivalRPG.java
+++ src/main/java/de/medivalrpg/MedivalRPG.java
@@ -1,5 +1,6 @@
 package de.medivalrpg;
 
+import de.medivalrpg.drinks.DrinkSystem;
 import de.medivalrpg.moneysystem.MoneySystem;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -7,8 +8,9 @@ public class MedivalRPG extends JavaPlugin {
     //Instance of class
     private MedivalRPG plugin;
 
-    //MoneySystem object
+    //System objects
     private MoneySystem moneySystem;
+    private DrinkSystem drinkSystem;
 
     private Initializer initializer;
 
@@ -17,10 +19,12 @@ public class MedivalRPG extends JavaPlugin {
         //Initialize instance of class
         this.plugin = this;
 
-        //Initialize moneySystem object
+        //Initialize System object
         this.moneySystem = new MoneySystem(plugin);
+        this.drinkSystem = new DrinkSystem(plugin);
 
         moneySystem.initialize();
+        drinkSystem.initialize();
 
         this.initializer = new Initializer(this.plugin);
 
--- src/main/java/de/medivalrpg/drinks/DrinkSystem.java
+++ src/main/java/de/medivalrpg/drinks/DrinkSystem.java
@@ -1,4 +1,23 @@
 package de.medivalrpg.drinks;
 
+import de.medivalrpg.MedivalRPG;
+import de.medivalrpg.drinks.Listener.DrinkListener;
+import de.medivalrpg.moneysystem.MoneyUtil;
+import org.bukkit.Bukkit;
+
 public class DrinkSystem {
+
+    private MedivalRPG plugin;
+    private MoneyUtil moneyUtil;
+
+    public DrinkSystem(MedivalRPG plugin) {
+        this.plugin = plugin;
+        this.moneyUtil = new MoneyUtil();
+    }
+
+    public void initialize() {
+        Bukkit.getPluginManager().registerEvents(new DrinkListener(), plugin);
+        //TODO register
+    }
+
 }
