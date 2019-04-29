ace16bb63b09c3ce7fec965fce521c5adaa50462
AzraAnimating <42267730+AzraAnimating@users.noreply.github.com>
2019-04-29 19:20:05 +0200

    Added Listener

--- src/main/java/de/medivalrpg/intromessages/IntroText.java
+++ src/main/java/de/medivalrpg/intromessages/IntroText.java
@@ -1,4 +1,10 @@
 package de.medivalrpg.intromessages;
 
+import org.bukkit.entity.Player;
+import org.bukkit.event.Listener;
+import org.bukkit.event.EventHandler;
+
 public class IntroText {
+
+
 }
