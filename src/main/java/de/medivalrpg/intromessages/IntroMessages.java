fb479d6b3ee59a58b7136403c6452101eb5e55b0
AzraAnimating <42267730+AzraAnimating@users.noreply.github.com>
2019-04-29 18:07:36 +0200

    Added Intro-Text

--- src/main/java/de/medivalrpg/intromessages/IntroMessages.java
+++ src/main/java/de/medivalrpg/intromessages/IntroMessages.java
@@ -6,14 +6,20 @@ public class IntroMessages {
 
     //System objectives
     private MedivalRPG plugin;
+    private IntroText introText;
 
     public IntroMessages(MedivalRPG plugin) {
         //Initialize system objectives
         this.plugin = plugin;
+        this.introText = new IntroText();
     }
 
     public void initialize() {
         //Register
         //TODO register
+    }
+
+    public void tutorial(){
+
     }
 }
---
+++ src/main/java/de/medivalrpg/intromessages/IntroText.java
@@ -0,0 +1,4 @@
+package de.medivalrpg.intromessages;
+
+public class IntroText {
+}
